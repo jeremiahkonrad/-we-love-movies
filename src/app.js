f4683dc81eaead9d634c4109454d818508e646ab
Jeremiah Bautista <jeremiah.bautista@gmail.com>
2024-04-28 12:58:25 -0700

    feat(payload structure): enables express' json middleware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const cors = require("cors");
-
 if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
 
 app.use(cors());
+app.use(express.json());
 
 module.exports = app;
